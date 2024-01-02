69bb8e7ef6044301a52fafde94ff830106473ced
Garr Godfrey <garr@hunkydoryrepair.com>
2024-01-02 01:36:15 -0800

    break out connection string for easier testing

--- Program.cs
+++ Program.cs
@@ -29,18 +29,25 @@ class MySyncContext : SynchronizationContext
 
 class Program
 {
+    // a valid connection string is necessary to reproduce.
+    // although asyncronous activity happens with a bad password, no errors
+    // have been reproduced unless a connection is succesfully done.
+    static String CONNECTIONSTRING = "Server=127.0.0.1;Port=3306;Database=plposdev;UID=devpos;Pwd=happy123;Charset=utf8mb4;ConnectionReset=true;maxpoolsize=1";
     static async Task Main(string[] args)
     {
         SynchronizationContext.SetSynchronizationContext(new MySyncContext());
         Console.WriteLine("Override Context {0}: Thread {1}", SynchronizationContext.Current?.GetType().Name ?? "null", Thread.CurrentThread.ManagedThreadId);
-        using (var conn = new MySqlConnection("Server=127.0.0.1;Port=3306;Database=plposdev;UID=devpos;Pwd=happy123;Charset=utf8mb4;ConnectionReset=true;maxpoolsize=1"))
+        using (var conn = new MySqlConnection(CONNECTIONSTRING))
         {
-            await conn.OpenAsync();
-            Console.WriteLine("After OpenAsync: {0}, Thread {1}", SynchronizationContext.Current?.GetType().Name ?? "null", Thread.CurrentThread.ManagedThreadId);
-            if (SynchronizationContext.Current == null)
+            try
             {
-                Console.WriteLine("**** Synchronization Context should still be MySyncContext! ****");
+                await conn.OpenAsync();
             }
+            catch (Exception)
+            {
+                Console.WriteLine("*** CONNECTION FAILURE. TEST INVALID ****");
+            }
+            Console.WriteLine("After OpenAsync: {0}, Thread {1}", SynchronizationContext.Current?.GetType().Name ?? "null", Thread.CurrentThread.ManagedThreadId);
         }
 
         // don't synchronize context.
@@ -48,14 +55,25 @@ class Program
         {
             // In our threadpool thread, we want a different context.
             Console.WriteLine("New Threadpool Thread Context {0}, Thread {1}", SynchronizationContext.Current?.GetType().Name ?? "null", Thread.CurrentThread.ManagedThreadId);
-            using (var conn = new MySqlConnection("Server=127.0.0.1;Port=3306;Database=plposdev;UID=devpos;Pwd=happy123;Charset=utf8mb4;ConnectionReset=true;maxpoolsize=1"))
+            using (var conn = new MySqlConnection(CONNECTIONSTRING))
             {
-                await conn.OpenAsync();
+                try
+                {
+                    await conn.OpenAsync();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("*** CONNECTION FAILURE. TEST INVALID ****");
+                }
                 Console.WriteLine("After OpenAsync: {0}, Thread: {1}", SynchronizationContext.Current?.GetType().Name ?? "null", Thread.CurrentThread.ManagedThreadId);
             }
             if (SynchronizationContext.Current != null)
             {
                 Console.WriteLine("**** Synchronization Context should still be NULL ****");
+            }
+            else
+            {
+                Console.WriteLine("No error detected this time. Please try again.");
             }
         });
     }
